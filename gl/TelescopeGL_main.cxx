7ddb79f2a32f0c73619660142012ba5ba446482c
Yi Liu <yi.liu@desy.de>
2020-06-24 15:30:58 +0200

    add telescope viewer class

--- gl/TelescopeGL_main.cxx
+++ gl/TelescopeGL_main.cxx
@@ -113,6 +113,7 @@ int main(int argc, char **argv){
   telgl.addTelLayer(0, 0, 120, 0.5, 0.5, 0, 0.028, 0.026, 1.0, 1024, 512);
   telgl.addTelLayer(0, 0, 150, 0, 0.5, 0.5, 0.028, 0.026, 1.0, 1024, 512);
   telgl.addTelLayer(0, 0, 180, 1, 0, 1, 0.028, 0.026, 1.0, 1024, 512);
+  telgl.addTelLayer(0, 0, 250, 0.5, 0, 0.5, 0.028, 0.026, 1.0, 1024, 512);
   telgl.buildProgramTel();
   telgl.buildProgramHit();
 
@@ -140,30 +141,21 @@ int main(int argc, char **argv){
           std::this_thread::sleep_for(std::chrono::milliseconds(100));
           break;
         }
-        ClusterPool cpool;
         telgl.clearHit();
-        for (auto& subev : ev_it->GetArray()){
-          PrintJson(subev);
-          for (auto& hit : subev["hit_xyz_array"].GetArray()){
-            telgl.addHit(hit[0].GetInt(), hit[1].GetInt(), hit[2].GetInt());            
-            cpool.addHit(hit[0].GetInt(), hit[1].GetInt(), hit[2].GetInt());
+        
+        const auto &js_ev = ev_it->GetObject();
+        
+        const auto &js_layers = js_ev["layers"].GetArray();        
+        std::cout<<"\n  "<<std::endl;
+        for(const auto& js_layer : js_layers){
+          JadeDataFrame df(js_layer);
+          PrintJson(js_layer);
+          std::printf("recreated native cluster: ");
+          for(auto &c : df.m_clusters){
+            telgl.addHit( c.x()/0.02924+0.1, c.y()/0.02688+0.1, c.z() );
+            std::printf(" [%f, %f, %u] ", c.x(), c.y(), c.z());
           }
-        }
-        std::cout<<std::endl;
-
-        cpool.buildClusters();
-
-        uint64_t nc = cpool.m_clusters.size();
-        for(uint64_t i = 0; i< nc; i++){
-          auto &cluster = cpool.m_clusters[i];
-          std::fprintf(stdout, "\n cluster@%u: [%f, %f, %u] \n", i, cluster.centerX, cluster.centerX, cluster.surfIndex);
-          for(auto& ph: cluster.pixelHits){
-            uint64_t hx = ph.x();
-            uint64_t hy = ph.y();
-            uint64_t hz = ph.z();
-            std::fprintf(stdout, "   [%u, %u, %u] ", hx, hy, hz);
-          }
-          std::fprintf(stdout, "\n");
+          std::printf(" \n");
         }
         
         telgl.clearFrame();
@@ -180,3 +172,139 @@ int main(int argc, char **argv){
     }
   }
 }
+
+
+
+
+class TelescopeViewer{
+
+public:
+  std::vector<JadeDataFrameSP> m_vec_ring_ev;
+  JadeDataFrameSP m_ring_end; // ring end is nullptr,therefore real data as nullptr should not go into the ring.  
+
+  uint64_t m_size_ring{200000};
+  std::atomic_uint64_t m_count_ring_write;
+  std::atomic_uint64_t m_count_ring_read;
+  std::atomic_uint64_t m_hot_p_read;
+  bool m_is_async_thread_running{false};  
+  std::future<uint64_t> m_fut_async_thread;
+  
+  ~TelescopeViewer() virtual {
+    stopAsyncThread();
+  }
+  
+  void clearBufferEvent(){ // by write thread
+    uint64_t count_ring_read = m_count_ring_read;
+    m_count_ring_write = count_ring_read;
+  }
+  
+  void pushBufferEvent(JadeDataFrameSP df){ //by write thread
+    uint64_t next_p_ring_write = m_count_ring_write % m_size_ring;
+    if(next_p_ring_write == m_hot_p_read){
+      std::fprintf(stderr, "buffer full, unable to write into buffer, monitor data lose\n");
+      return;
+    }
+    m_vec_ring_ev[next_p_ring_write] = df;
+    m_count_ring_write ++;
+  }
+  
+  JadeDataFrameSP& frontBufferEvent(){ //by read thread
+    if(m_count_ring_write > m_count_ring_read) {
+      uint64_t next_p_ring_read = m_count_ring_read % m_size_ring;
+      m_hot_p_read = next_p_ring_read;
+      // keep hot read to prevent write-overlapping
+      return m_vec_ring_ev[next_p_ring_read];
+    }
+    else{
+      return m_ring_end;
+    }
+  }
+
+  void popFrontBufferEvent(){ //by read thread
+    if(m_count_ring_write > m_count_ring_read) {
+      uint64_t next_p_ring_read = m_count_ring_read % m_size_ring;
+      m_hot_p_read = next_p_ring_read;
+      // keep hot read to prevent write-overlapping
+      m_vec_ring_ev[next_p_ring_read].reset();
+      m_count_ring_read ++;
+    }
+  }
+
+  void stopAsyncThread(){
+    m_is_async_thread_running = false;
+    if(m_fut_async_thread.valid())
+      m_fut_async_thread.get();
+  }
+
+  void startAsyncThread(){
+    if(m_is_async_thread_running){
+      std::fprintf(stderr, "unable to start up new async thread.  already running\n");
+    }
+    m_vec_ring_ev.clear();
+    m_vec_ring_ev.resize(m_size_ring);
+    m_count_ring_write = 0;
+    m_count_ring_read = 0;
+    m_hot_p_read = m_size_ring -1; // tail
+    
+    m_fut_async_thread = std::async(std::launch::async, &TelescopeViewer::async_gl_loop, this);
+  }
+  
+  uint64_t async_gl_loop(){
+    bool &flag_running = m_is_async_thread_running;
+    flag_running = true;
+
+    TelescopeGL telgl;
+    telgl.addTelLayer(0, 0, 0,   1, 0, 0,      0.028, 0.026, 1.0, 1024, 512);
+    telgl.addTelLayer(0, 0, 30,  0, 1, 0,      0.028, 0.026, 1.0, 1024, 512);
+    telgl.addTelLayer(0, 0, 60,  0, 0, 1,      0.028, 0.026, 1.0, 1024, 512);
+    telgl.addTelLayer(0, 0, 120, 0.5, 0.5, 0,  0.028, 0.026, 1.0, 1024, 512);
+    telgl.addTelLayer(0, 0, 150, 0, 1, 1,      0.028, 0.026, 1.0, 1024, 512);
+    telgl.addTelLayer(0, 0, 180, 1, 0, 1,      0.028, 0.026, 1.0, 1024, 512);
+    telgl.addTelLayer(0, 0, 240, 0.5, 0, 0.5,  0.028, 0.026, 1.0, 1024, 512);
+    telgl.buildProgramTel();
+    telgl.buildProgramHit();
+    
+    uint64_t n_gl_frame = 0;
+    bool flag_pausing = false;
+    sf::Event windowEvent;
+    while ( flag_running ){ 
+      while (telgl.m_window->pollEvent(windowEvent)){
+        switch (windowEvent.type){
+        case sf::Event::Closed:
+          flag_running = false;
+          break;
+        case sf::Event::KeyPressed:
+          flag_pausing = true;
+          break;
+        case sf::Event::KeyReleased:
+          flag_pausing = false;
+          break;
+        }
+      }
+      if(flag_pausing){
+        continue;
+      }
+    
+      telgl.clearHit();
+      
+      auto &dfev_ref = frontBufferEvent(); //ref only,  no copy, no move
+      if(dfev_ref){// not nullptr/ring_end
+        auto dfev = std::move(dfev_ref); //moved
+        popFrontBufferEvent();
+        for(auto &chit : dfev->m_clusters){
+          //only get first pixel hit from each cluster hit 
+          telgl.addHit(chit.pixelHits[0].x(),
+                       chit.pixelHits[0].y(),
+                       chit.pixelHits[0].z());
+        }
+      } 
+      telgl.clearFrame();
+      telgl.drawTel();
+      telgl.drawHit();
+      telgl.flushFrame();
+      n_gl_frame ++;
+    }
+    flag_running = false;
+    return n_gl_frame;
+  }
+};
